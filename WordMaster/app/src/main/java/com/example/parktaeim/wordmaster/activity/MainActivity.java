878d966af52a585b33ad776b74f0864602eb65d9
taeiim <ti535666@gmail.com>
2017-08-17 23:48:21 +0900

    Create WordBookList Adapter

--- WordMaster/app/src/main/java/com/example/parktaeim/wordmaster/activity/MainActivity.java
+++ WordMaster/app/src/main/java/com/example/parktaeim/wordmaster/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.parktaeim.wordmaster;
+package com.example.parktaeim.wordmaster.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.parktaeim.wordmaster.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ WordMaster/app/src/main/java/com/example/parktaeim/wordmaster/adapter/WordBookListAdapter.java
@@ -0,0 +1,8 @@
+package com.example.parktaeim.wordmaster.adapter;
+
+/**
+ * Created by parktaeim on 2017. 8. 17..
+ */
+
+public class WordBookListAdapter {
+}
