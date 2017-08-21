1af07c43ae7edba6120888de60eadfbbac5679d3
taeiim <ti535666@gmail.com>
2017-08-22 01:15:53 +0900

    Create Toolbar

--- WordMaster/app/src/main/java/com/example/parktaeim/wordmaster/activity/MainActivity.java
+++ WordMaster/app/src/main/java/com/example/parktaeim/wordmaster/activity/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.parktaeim.wordmaster.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.example.parktaeim.wordmaster.R;
 
@@ -11,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //툴바
+        setSupportActionBar((Toolbar)findViewById(R.id.toolbar));
+        getSupportActionBar().setTitle("");  //툴바 제목 설정 (제거)
+
     }
 }
