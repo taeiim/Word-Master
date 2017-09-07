bf76783172aeee7b27e75647b0bbab605b91b744
taeiim <ti535666@gmail.com>
2017-09-07 19:50:12 +0900

    Realm with RecyclerView and CardView in MainActivity

--- WordMaster/app/src/main/java/com/example/parktaeim/wordmaster/activity/MainActivity.java
+++ WordMaster/app/src/main/java/com/example/parktaeim/wordmaster/activity/MainActivity.java
@@ -10,16 +10,21 @@ import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.parktaeim.wordmaster.R;
@@ -28,11 +33,13 @@ import com.example.parktaeim.wordmaster.model.WordBook;
 import com.getkeepsafe.relinker.ReLinker;
 
 import java.util.Date;
+
 import io.realm.Realm;
 import butterknife.ButterKnife;
 import co.moonmonkeylabs.realmrecyclerview.RealmRecyclerView;
 import io.realm.RealmResults;
 import io.realm.RealmConfiguration;
+import io.realm.Sort;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -40,9 +47,11 @@ public class MainActivity extends AppCompatActivity {
     private String title;
     private String describe;
 
+    Dialog bookAddDialog;
     private Realm realm;
-
-    RealmRecyclerView mRecyclerView;
+    WordBookRealmAdapter wordBookRealmAdapter;
+    RealmResults<WordBook> wordBooksItems;
+    RecyclerView realmRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,37 +59,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        Realm.init(getApplicationContext());
+        Realm.init(this);
         realm = Realm.getDefaultInstance();
 
-        //realm = Realm.getInstance(this);
+        realmRecyclerView = (RecyclerView) findViewById(R.id.WordBookRecyclerView);
 
-//        RealmConfiguration config = new RealmConfiguration.Builder(getApplicationContext()).build();
-//        realm = Realm.getInstance(config);
-
-//        Realm.init(getApplicationContext());
-//        RealmConfiguration realmConfig = new RealmConfiguration.Builder().name("realm").build();
-//        realm = Realm.getInstance(realmConfig);
-
-//        ReLinker.loadLibrary(getApplicationContext(),"mylibrary","1.0");
-
-//        Realm.init(getApplicationContext());
-//        realm = Realm.getDefaultInstance();
-//
-
-        RealmResults<WordBook> wordBooksItems = realm
-                .where(WordBook.class)
-                .findAllSorted("list_id");
-
-        WordBookRealmAdapter wordBookRealmAdapter = new WordBookRealmAdapter(this,wordBooksItems,true,true);
-        RealmRecyclerView realmRecyclerView = (RealmRecyclerView) findViewById(R.id.WordBookRecyclerView);
-        realmRecyclerView.setAdapter(wordBookRealmAdapter);
-
+        setUpRecyclerView();
 
         ButterKnife.bind(this);
 
         setToolbar();  //툴바 세팅
-
 
 
         Button button = (Button) findViewById(R.id.intentBtn);
@@ -97,11 +85,21 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this,WordsActivity.class);
+                Intent intent = new Intent(MainActivity.this, WordsActivity.class);
                 startActivity(intent);
             }
         });
 
+
+    }
+
+    private void setUpRecyclerView() {
+        wordBookRealmAdapter= new WordBookRealmAdapter(realm.where(WordBook.class).findAllSorted("list_id", Sort.DESCENDING));
+        realmRecyclerView.setAdapter(wordBookRealmAdapter);
+        realmRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        realmRecyclerView.setHasFixedSize(true);
+        realmRecyclerView.addItemDecoration(new DividerItemDecoration(this,DividerItemDecoration.VERTICAL));
+        wordBookRealmAdapter.notifyDataSetChanged();
 
     }
 
@@ -112,38 +110,33 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void setToolbar() {
-        setSupportActionBar((Toolbar)findViewById(R.id.toolbar));
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
         getSupportActionBar().setTitle("");  //툴바 제목 설정 (제거)
     }
 
 
-
-
     //단어장 추가 다이얼로그
-    private void ShowDialog(){
-        LayoutInflater dialog = LayoutInflater.from(this);
-        final View dialogLayout = dialog.inflate(R.layout.word_book_dialog,null);
-        final Dialog bookAddDialog = new Dialog(this);
+    private void ShowDialog() {
+        final LayoutInflater dialog = LayoutInflater.from(this);
+        final View dialogLayout = dialog.inflate(R.layout.word_book_dialog, null);
+        bookAddDialog = new Dialog(this);
 
         bookAddDialog.setContentView(dialogLayout);
         bookAddDialog.show();
         bookAddDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
 
-        Button btn_add = (Button) dialogLayout.findViewById(R.id.btn_add);
+        Button btn_add_wordBook = (Button) dialogLayout.findViewById(R.id.btn_add_wordBook);
         Button btn_cancel = (Button) dialogLayout.findViewById(R.id.btn_cancel);
-        final EditText titleEditText = (EditText) findViewById(R.id.titleEditText);
-        final EditText descEditText = (EditText) findViewById(R.id.descEditText);
+        final EditText titleEditText = (EditText) dialogLayout.findViewById(R.id.titleEditText);
+        final EditText descEditText = (EditText) dialogLayout.findViewById(R.id.descEditText);
 
 
         //추가 버튼 클릭했을 때
-        btn_add.setOnClickListener(new View.OnClickListener() {
+        btn_add_wordBook.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if( titleEditText == null || descEditText == null||titleEditText.length() == 0 || descEditText.length() == 0 ){   //제목이나 요약 입력값이 없으면 다시 작성해달라는 토스트
-                    Toast.makeText(MainActivity.this, "다시 작성해주세요!",Toast.LENGTH_SHORT).show();
-                }else {
-                    addWordBookItem(titleEditText.getText().toString(),descEditText.getText().toString());
-                }
+                    addWordBookItem(titleEditText.getText().toString(), descEditText.getText().toString());
+
             }
         });
 
@@ -159,22 +152,27 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    private void addWordBookItem(String title, String describe){
+    private void addWordBookItem(String title, String describe) {
         if (title == null || title.length() ==0 ||describe == null || describe.length() ==0 ){
             Toast.makeText(MainActivity.this, "다시 작성해주세요!",Toast.LENGTH_SHORT).show();
             return;
         }
 
-        long now = System.currentTimeMillis();
-        Date date = new Date(now);
+        wordBooksItems = realm.where(WordBook.class).findAll();
 
+        System.out.println(wordBooksItems.size());
 
         realm.beginTransaction();
-        WordBook wordBook = realm.createObject(WordBook.class);
-        wordBook.setList_id(System.currentTimeMillis());
+        WordBook wordBook = realm.createObject(WordBook.class,System.currentTimeMillis());
         wordBook.setTitle(title);
         wordBook.setDesc(describe);
         realm.commitTransaction();
+        bookAddDialog.dismiss();
+
+        System.out.println(wordBooksItems.size());
+
+
+
     }
 
 
--- WordMaster/app/src/main/java/com/example/parktaeim/wordmaster/adapter/WordBookRealmAdapter.java
+++ WordMaster/app/src/main/java/com/example/parktaeim/wordmaster/adapter/WordBookRealmAdapter.java
@@ -1,7 +1,10 @@
 package com.example.parktaeim.wordmaster.adapter;
 
 import android.content.Context;
+import android.support.annotation.Nullable;
 import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -9,8 +12,14 @@ import android.widget.TextView;
 import com.example.parktaeim.wordmaster.R;
 import com.example.parktaeim.wordmaster.model.WordBook;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import io.realm.OrderedRealmCollection;
 import io.realm.Realm;
 import io.realm.RealmBasedRecyclerViewAdapter;
+import io.realm.RealmConfiguration;
+import io.realm.RealmRecyclerViewAdapter;
 import io.realm.RealmResults;
 import io.realm.RealmViewHolder;
 
@@ -18,39 +27,62 @@ import io.realm.RealmViewHolder;
  * Created by parktaeim on 2017. 9. 1..
  */
 
-public class WordBookRealmAdapter extends RealmBasedRecyclerViewAdapter<WordBook,WordBookRealmAdapter.ViewHolder> {
+public class WordBookRealmAdapter extends RealmRecyclerViewAdapter<WordBook,WordBookRealmAdapter.ViewHolder> {
+
+    Context context;
+    Realm realm;
+    protected OrderedRealmCollection<WordBook> items;
+    private RecyclerView mAttechedRecyclerView;
 
     public class ViewHolder extends RealmViewHolder {
 
+        public CardView cardView;
         public TextView titleTextView;
         public TextView descTextView;
 
         public ViewHolder(CardView container) {
             super(container);
+            this.cardView = (CardView) container.findViewById(R.id.wordBookCardView);
             this.titleTextView = (TextView) container.findViewById(R.id.titleTextView);
             this.descTextView = (TextView) container.findViewById(R.id.describeTextView);
         }
     }
 
-    public WordBookRealmAdapter(
-            Context context,
-            RealmResults<WordBook> realmResults,
-            boolean automaticUpdate,
-            boolean animateResults) {
-        super(context, realmResults, automaticUpdate, animateResults);
+
+    public WordBookRealmAdapter(OrderedRealmCollection<WordBook> data){
+        super(data,true);
+        this.items = data;
+        setHasStableIds(true);
     }
 
     @Override
-    public ViewHolder onCreateRealmViewHolder(ViewGroup viewGroup, int viewType) {
-        View v = inflater.inflate(R.layout.wordsbookliist_custom, viewGroup, false);
+    public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+        super.onAttachedToRecyclerView(recyclerView);
+        mAttechedRecyclerView = recyclerView;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.wordsbookliist_custom,mAttechedRecyclerView,false);
         ViewHolder vh = new ViewHolder((CardView) v);
         return vh;
     }
 
     @Override
-    public void onBindRealmViewHolder(ViewHolder viewHolder, int position) {
-        final WordBook wordBookItem = realmResults.get(position);
-        viewHolder.titleTextView.setText(wordBookItem.getTitle());
-        viewHolder.descTextView.setText(wordBookItem.getDesc());
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        final WordBook wordBookItem = items.get(position);
+        holder.titleTextView.setText(wordBookItem.getTitle());
+        holder.descTextView.setText(wordBookItem.getDesc());
+    }
+
+
+    @Override
+    public long getItemId(int index) {
+        return super.getItemId(index);
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
     }
 }
